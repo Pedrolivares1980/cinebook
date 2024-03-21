c7e15cec7adedfd09f404beb26e37e3c3587602f
Pedro <pedr.olivares80@gmail.com>
2024-03-21 13:54:43 +0000

    profile

--- users/signals.py
+++ users/signals.py
@@ -6,7 +6,8 @@ from .models import Profile
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        profile = Profile(user=instance)
+        profile.save()
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
