4cbacf8151b9a83af9dde57d5b8f777f2c09ed42
Pedro <pedr.olivares80@gmail.com>
2024-03-21 14:09:06 +0000

    users/ models

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from io import BytesIO
 from django.contrib.auth.models import User
+from django.core.files.storage import default_storage as storage
 from PIL import Image
 
 class Profile(models.Model):
@@ -12,9 +14,23 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+        # Open the image using the storage backend
+        with storage.open(self.image.name, 'rb') as image_file:
+            img = Image.open(image_file)
 
-        if img.height > 200 or img.width > 200:
-            output_size = (200, 200)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+            if img.height > 200 or img.width > 200:
+                output_size = (200, 200)
+                img.thumbnail(output_size)
+                
+                # Determine the image format (fallback to JPEG if unknown)
+                img_format = img.format if img.format else 'JPEG'
+                
+                # Save the modified image to a BytesIO/stream
+                stream = BytesIO()
+                img.save(stream, format=img_format)
+                stream.seek(0)
+                
+                # Save the image back to the storage
+                self.image.save(self.image.name, content=stream, save=False)
+
+        super().save(*args, **kwargs)
