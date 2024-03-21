b4175f57e8d4aa87232275d92bc9837a9681f469
Pedro <pedr.olivares80@gmail.com>
2024-03-21 12:47:55 +0000

    debuging

--- cinebook/settings.py
+++ cinebook/settings.py
@@ -16,7 +16,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') == 'TRUE'
+# DEBUG = os.environ.get('DEBUG') == 'TRUE'
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('RENDER_HOSTNAME', '')]
 
